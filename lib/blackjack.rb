af9a8dc6afc2987f6002a873bb3200065b940e89
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-04-19 21:48:52 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,15 +38,7 @@ end
 def hit?(num)
   # code hit? here
   prompt_user
-  user_input = get_user_input
-    if user_input == "h"
-      num += deal_card
-    elsif user_input == "s"
-    display_card_total(num)
-  else 
-    invalid_command
-  end
-  num
+
 end
 
 def invalid_command
